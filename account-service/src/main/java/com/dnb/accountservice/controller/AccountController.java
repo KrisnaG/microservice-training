3113806e48a6b4cb29f6d849ca37a8ac6922ddd4
Krisna <>
2023-10-26 10:40:30 +0530

    Update

--- account-service/src/main/java/com/dnb/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/dnb/accountservice/controller/AccountController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +44,7 @@ public class AccountController {
 	private EntityToResponseMapper entityToResponseMapper;
 
 	@PostMapping("/create")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	public ResponseEntity<?> createAccount(@Valid @RequestBody AccountRequest accountRequest)
 			throws IdNotFoundException, AccountMappingException {
 
